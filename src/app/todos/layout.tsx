a0b5c2df3dfef5d1f8ed4cd85b278f90dec8adf8
Jun Pyo Seo <cooljp95@naver.com>
2023-08-11 21:43:10 +0900

    [feat] 로그인 시 Todos에 접근가능하도록 설정

--- src/app/todos/layout.tsx
+++ src/app/todos/layout.tsx
@@ -1,6 +1,13 @@
+import { authOptions } from '@/lib/nextAuth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const layout = ({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) => {
+const layout = async ({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) redirect('/auth/login');
+
   return (
     <>
       {children}
